ea1e3d8f61eea2cdffb61ec89021a6674f90a9fb
ivtarasenkov <ivtarasenkov@gmail.com>
2024-11-22 13:40:08 +0300

    Delete Dict #1.py

--- Dict #1.py
+++
@@ -1,7 +0,0 @@
-a = {
-     "r1" :  {"location":"21 New Globe Walk","vendor":"Cisco","model":"4451","ios":"15.4","ip":"10.255.0.1"},
-     "r2" :  {"location":"21 New Globe Walk","vendor":"Cisco","model":"4451","ios":"15.4","ip":"10.255.0.2"},
-     "sw1" : {"location":"21 New Globe Walk","vendor":"Cisco","model":"3850","ios":"3.6.XE","ip": "10.255.0.101","vlans":"10,20,30","routing":"True"}
-    }
-b=input('Введите название устройства: ')
-print(a[b])
